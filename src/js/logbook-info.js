4ea073d2050bf3c1583931fa3df65d51fb9a54ac
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2021-01-18 15:42:56 +0300

    Передача значкений в поле статус  и цель поездки

--- src/js/logbook-info.js
+++ src/js/logbook-info.js
@@ -10,6 +10,8 @@ const LOGBOOKINFO_CLASS_SHOW = 'logbook__info--show'
 const testDriveField = logbookInfo.querySelector('.js-testDrive')
 const testDriveTimeFromField = logbookInfo.querySelector('.js-testDriveTimeFrom')
 const testDriveTimeToField = logbookInfo.querySelector('.js-testDriveTimeTo')
+const testDriveStatus = logbookInfo.querySelector('.js-testDriveStatus')
+const testDriveType = logbookInfo.querySelector('.js-testDriveType')
 
 const clearLogbookInfo = () => {
   testDriveField.value = ''
@@ -26,10 +28,43 @@ const updateLogbookInfo = (data) => {
   const departmentsList = data.departmentsList
 
   testDriveField.value = carInfo.UF_TEST_DRIVE
+  testDriveStatus.value = 'Записан'
+  testDriveType.value = 'Тест-драйв'
 
   if (!!carLogbookInfo.ID) {
     testDriveTimeFromField.value = carLogbookInfo.UF_DATE_FROM.slice(0, -3)
     testDriveTimeToField.value = carLogbookInfo.UF_DATE_TO.slice(0, -3)
+
+    switch (carLogbookInfo.UF_STATUS) {
+      case 'Пройден':
+        testDriveStatus.value = 'Пройден'
+        break
+      case 'Отменён':
+        testDriveStatus.value = 'Отменён'
+        break
+      case 'Запланирован':
+        testDriveStatus.value = 'Записан'
+        break
+      default:
+        testDriveStatus.value = 'Записан'
+    }
+
+    switch (carLogbookInfo.UF_TYPE) {
+      case 'Тест-драйв':
+        testDriveType.value = 'Тест-драйв'
+        break
+      case 'Длительный тест-драйв':
+        testDriveType.value = 'Длительный тест-драйв'
+        break
+      case 'Служебная':
+        testDriveType.value = 'Служебная'
+        break
+      case 'Перемещение':
+        testDriveType.value = 'Перемещение'
+        break
+      default:
+        testDriveType.value = 'Тест-драйв'
+    }
 
     console.info(carLogbookInfo.UF_TYPE)
   } else {
@@ -72,28 +107,27 @@ const openLogbookInfo = (evt, data) => {
   //
   // autoTest.value = evt.target.dataset.carTd
   //
-  // const purposeOfTrip = document.querySelector('.js-purposeOfTrip')
-  // const playground = document.querySelector('.js-playground')
-  //
+  const purposeOfTrip = document.querySelector('.js-purposeOfTrip')
+  const playground = document.querySelector('.js-playground')
+
   // playgroundList.forEach((item) => {
   //   const html = `<option class="form-select__optional" value="${item[0]}">${item[1]}</option>`
   //
   //   playground.innerHTML += html
   // })
-  //
-  // const changeShroud = () => {
-  //   if (purposeOfTrip.value !== 'Move') {
-  //     playground.setAttribute('disabled', 'disabled')
-  //   } else {
-  //     playground.removeAttribute('disabled')
-  //   }
-  // }
-  // changeShroud()
-  //
-  // purposeOfTrip.addEventListener('change', () => {
-  //   changeShroud()
-  // })
-  //
+
+  const changeShroud = () => {
+    if (purposeOfTrip.value !== 'Перемещение') {
+      playground.setAttribute('disabled', 'disabled')
+    } else {
+      playground.removeAttribute('disabled')
+    }
+  }
+  changeShroud()
+
+  purposeOfTrip.addEventListener('change', () => {
+    changeShroud()
+  })
 
   closeBtnLogbookInfo.addEventListener('click', (evt) => {
     if (isLogbookInfoShow) {
